23e2ffa84635d0be9ee9a69a61a3fc76731ebbe3
ynishimi <nishimi@Nishimi.localdomain>
2022-05-23 10:58:47 +0900

    quiz1.cの完成？

--- test.c
+++ test.c
@@ -1,7 +1,15 @@
 #include "nn.h"
+
 void print(int m, int n, const float * x)
 {
-    
+    int i, j;
+    for (i = 0; i < n; i++)
+    {
+        for(j = 0; j < m; j++)
+        {
+            printf("%f\n, &x[0]");
+        }
+    }
 }
 
 int main()
