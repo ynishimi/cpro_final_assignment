26ac037ebc33edd3ef5fbaa3717aa77693b3aa9c
ynishimi <atasttn@icloud.com>
2022-05-23 16:38:53 +0900

    test.cの変更

---
+++ test.c
@@ -0,0 +1,18 @@
+#include "nn.h"
+void print(int m, int n, const float *x)
+{
+    int i, j;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j<m; j++)
+        {
+            x[j]
+        }
+    }
+}
+// テスト
+int main()
+{
+    print(1, 10, b_784x10);
+    return 0;
+}
